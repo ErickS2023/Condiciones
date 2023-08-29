a1ccd7f1e41a8275657968f14dd92f04db393440
ErickS2023 <141955911+ErickS2023@users.noreply.github.com>
2023-08-29 12:35:26 -0600

    Add files via upload

---
+++ Asignacion 2.cpp
@@ -0,0 +1,18 @@
+//*Programa que calcula algunas expresiones logicas*/
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+	//expresiones logicas
+
+    printf("La expresion logica de 1: %d\n",(5>3 && 10 <=10));
+    printf("La expresion logica de 2: %d\n",(50>75 || 100 <57));
+   printf("La expresion logica de 3: %d\n",5 >=3);
+    printf("La expresion logica de 4: %d\n",!(5 >=3));
+    printf("La expresion logica de 5: %d\n",!(5 <=3));
+
+   	printf("\a");//Alerta
+   
+    return 0; 
+}
+
---
+++ Condiciones.cpp
@@ -0,0 +1,25 @@
+//Programa crear una condicion 
+#include <stdio.h>
+#include <Math.h>
+int main()
+{
+	int a=30;
+	if( a==100)
+	{
+		printf("El valor es 10/n");
+		
+	}
+		else 
+		if (!(a == 20))
+		{
+		
+		   printf("El valor es 20/n");	
+		} 
+		else
+		{
+			 printf("No es 10 o 20/n");
+		}
+		 		
+}
+
+//if(!(0> 3 &&10 <=10))
